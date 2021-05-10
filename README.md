cbc9926924fef6e970b66e9a231a1e09d1745fba
aiwasi <siosazanami@gmail.com>
2021-05-10 16:18:10 +0900

    タブ機能とRSS/RDF登録機能を追加

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 RSSを読み込んで一覧表示，押すと記事が開くやつ<br>
 深夜テンションとノリで作ったので出来は察してください<br><br>
 TkinterでGUI表示を行っています
+
+## 使い方
+1. Pythonを入れる
+1. feedparserを入れる
+1. python main.pyをする
+
 ## 追加のモジュールなど
 - feedparser<br>
 RSSを読み込むために使います
--- main.py
+++ main.py
@@ -1,42 +1,87 @@
 import tkinter as tk
-from tkinter import ttk
+import tkinter.ttk as ttk
 import feedparser
 import webbrowser
+import csv
 
-#RSSからニュース情報取得
-rss_url = "https://kahoku.news/rss/general/%E5%AE%AE%E5%9F%8E" #任意のRSS/RDF
-rss_dic = feedparser.parse(rss_url)
-newstxt = []
-newslink = []
-for entry in rss_dic.entries:
-    title = entry.title
-    link = entry.link
-    newstxt.append(title)
-    newslink.append(link)
+def main():
+    #ウィンドウ作成
+    root = tk.Tk()
+    root.title("tk_RSSreader")
+    root.geometry("1200x600")
 
-#ウィンドウ作成
-root = tk.Tk()
-root.title("今日のニュース")
+    #ノートブック作成
+    nb=ttk.Notebook()
+    nb.pack(fill='both',expand=1)
 
-#リストボックス収納用フレーム作成
-frame = tk.Frame()
-frame.grid(row=0)
+    #ホームタブ作成
+    tab = tk.Frame(nb)
+    nb.add(tab,text="ホーム", padding=3)
+    
+    #ホームタブ用フレーム
+    frm=ttk.Frame(tab)
+    frm.grid()
 
-#ニュースのソース媒体名を表示
-news_source = ttk.Label(frame, text=rss_dic.feed.title + "からのニュース")
-news_source.grid(column=0, row=0)
+    #説明
+    label = tk.Label(frm, text="RSS/RDFのURLを入力(本ソフト再起動後に反映されます)")
+    label.grid(column=0,row=0)
 
-#リストボックス作成・設置
-txt = tk.StringVar(value=newstxt)
-lb = tk.Listbox(frame, listvariable=txt, height=16, width=55, selectmode="single",)
-lb.grid(column=0, row=1)
+    #RSS/RDFのURL入力用ボックス
+    txtbox = tk.Entry(frm,width=100)
+    txtbox.grid(column=0,row=1)
 
-#選択したタイトルの記事をブラウザで開く
-def webopen(event):
-    for i in lb.curselection():
-        webbrowser.open(newslink[i])
+    #CSV書き込み用ボタン
+    btn = tk.Button(frm, text='登録',command=lambda: csvwrite(txtbox.get()))
+    btn.grid(column=1,row=1)
+    
+    #CSVファイル読み込み・ニュースタブ作成
+    with open('csv/index.csv') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            tab = tk.Frame(nb)
+            nb.add(tab,text=row[1], padding=3)
+            newstab(row, tab)
 
-#リストボックスの中身を選択したらイベント実行
-lb.bind("<<ListboxSelect>>", webopen) 
+    root.mainloop()
+    return 0
 
-root.mainloop()
+def csvwrite(rss_url): #入力されたURLをCSVに書き込み
+    rss_dic = feedparser.parse(rss_url)
+    tabtitle = rss_dic.feed.title #RSSの見出し
+    tabtitle = ''.join(tabtitle.split()) #見出しの空白文字を削除
+    with open('csv/index.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([rss_url, tabtitle])
+    return 0
+
+def newstab(row, tab): #RSSからニュース情報取得
+    rss_url = row[0] #任意のRSS/RDF
+    rss_dic = feedparser.parse(rss_url)
+    newstxt = []
+    newslink = []
+    for entry in rss_dic.entries:
+        title = entry.title
+        link = entry.link
+        newstxt.append(title)
+        newslink.append(link)
+
+    #ニュースのソース媒体名を表示
+    news_source = ttk.Label(tab, text=rss_dic.feed.title + "からのニュース")
+    news_source.grid(column=0, row=0)
+
+    #リストボックス作成・設置
+    txt = tk.StringVar(value=newstxt)
+    lb = tk.Listbox(tab, listvariable=txt, height=48, width=192, selectmode="single")
+    lb.grid(column=0, row=1)
+
+    #選択したタイトルの記事をブラウザで開く
+    def webopen(event):
+        for i in lb.curselection():
+            webbrowser.open(newslink[i])
+
+    #リストボックスの中身を選択したらイベント実行
+    lb.bind("<<ListboxSelect>>", webopen) 
+    return 0
+
+if __name__ == "__main__":
+    main()
